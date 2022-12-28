3242e91080b570d76f10635774749bdc713080b7
Viciot <vfuentesnoguera@gmail.com>
2022-12-28 19:55:46 +0100

    Routes working :D

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
 import './App.css';
-import Home from "./pages/Home"
-import Rooms from "./pages/Rooms"
-import SingleRoom from "./pages/SingleRoom"
-import ErrorPage from "./pages/ErrorPage"
 
 
 
