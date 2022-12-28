9e9ee5c9447e5f3b1391eb611821204ad45c4b96
Viciot <vfuentesnoguera@gmail.com>
2022-12-28 18:24:38 +0100

    React Router creation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
-// import './App.css';
+import './App.css';
+import Home from "./pages/Home"
+import Rooms from "./pages/Rooms"
+import SingleRoom from "./pages/SingleRoom"
+import ErrorPage from "./pages/ErrorPage"
+
+
 
 function App() {
-  return <>Hello from app</>;  
+  return <>
+    <Home></Home>
+    <Rooms></Rooms>
+    <SingleRoom></SingleRoom>
+    <ErrorPage></ErrorPage>
+  </>;  
 }
 
 export default App;
