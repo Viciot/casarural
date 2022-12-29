41f00cd97a2de5a0bd8b2e23fbca13eff065903e
Viciot <vfuentesnoguera@gmail.com>
2022-12-29 17:09:28 +0100

    3rd day

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import './App.css';
 
+import Home from './pages/Home';
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import ErrorPage from "./pages/ErrorPage";
 
+
+import { 
+  BrowserRouter,
+  Routes,
+  Route 
+} from "react-router-dom";
 
 function App() {
-  return <>
-    <Home></Home>
-    <Rooms></Rooms>
-    <SingleRoom></SingleRoom>
-    <ErrorPage></ErrorPage>
-  </>;  
+  return (
+      <>
+        <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/rooms/" element={<Rooms />} />
+                <Route exact path="/rooms/:slug" element={<SingleRoom />} />  
+                {/* slug is a cappuccino name */}
+        </Routes>
+        
+      </>
+  );
 }
 
 export default App;
