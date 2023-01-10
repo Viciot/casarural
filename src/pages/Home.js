431484af190b71a0d747a07c4f075981450fe795
Viciot <vfuentesnoguera@gmail.com>
2023-01-10 11:49:52 +0100

    services and rooms

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,18 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
+import Services from "../components/Services";
 
 export default function Home () {
-    return <Hero>
-        <Banner title="Experimenta lo natural" subtitle="Residencias completas a partir de 199€">
-        <Link to="/rooms" className="btn-primary">
-        Nuestras Residencias
-        </Link>
-        </Banner>
-    </Hero>
+    return (
+        <>
+            <Hero>
+                <Banner title="Experimenta lo natural" subtitle="Residencias completas a partir de 199€">
+                    <Link to="/rooms" className="btn-primary">
+                        Nuestras Residencias
+                    </Link>
+                </Banner>
+            </Hero>
+        </>
+    );
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Hero from "../components/Hero"
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
+
 
 const Rooms = () => {
   return (
-    <Hero hero="roomsHero" />
+    <Hero hero="roomsHero">
+      <Banner title="Nuestros alojamientos">
+        <Link to="/" className="btn-primary">
+          Volver
+        </Link>
+      </Banner>
+    </Hero>
   )
 }
 
