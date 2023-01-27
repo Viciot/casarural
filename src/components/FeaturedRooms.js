ba88e2295aa1c08cd796c2640e05533a2b3af476
Viciot <vfuentesnoguera@gmail.com>
2023-01-27 12:12:08 +0100

    loading created

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
-
+import Loading from "./Loading";
 
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
   render() {
-    const { name, greeting } = this.context;
-    
+    const { featuredRooms: rooms } = this.context
+    console.log(rooms);
 
     return (
       
       <div>
-        {greeting} {name} from featured rooms
+         Hello from featured rooms
+
+        <Loading/>
       </div>
     )
   }
