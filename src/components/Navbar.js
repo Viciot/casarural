dd090dba63d437e6cd7c82d741ae77d2b087faaf
Viciot <vfuentesnoguera@gmail.com>
2023-01-27 12:41:34 +0100

    logo done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default class Navbar extends Component {
       <nav className="navbar">
         <div className="nav-center">
             <div className="nav-header">
-                <Link to="/">
+                <Link to="/">                    
                     <img src={logo} alt="Costanilla Rural" />
                 </Link>
                 <button type="button" className="nav-btn" onClick={this.handleToggle}>
