756dacc84deeadeaf494df7216e562aa65f368fc
Viciot <vfuentesnoguera@gmail.com>
2023-02-14 18:36:11 +0100

    roomslist

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -6,15 +6,19 @@ export default function RoomsList({rooms}) {
 if(rooms.lenght === 0){
   return (
     <div className="empty-search">
-      <h3>No hay coincidencias con los parámetros del filtro</h3>
+      <h3>No hay coincidencias con los parámetros de búsqueda</h3>
     </div>
   )
 }
 
 
   return (
-    <div>
-      Hello from room list
-    </div>
+    <section className="roomslist">
+      <div className="roomslist-center"> 
+        {rooms.map(item => {
+            return <Room key={item.id} room={item} />
+          })}
+      </div>
+    </section>
   )
 }
