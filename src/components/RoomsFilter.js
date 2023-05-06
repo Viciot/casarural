fbb0cbf707ca58367d3f2e00866f1ca16a2f1ba4
Viciot <vfuentesnoguera@gmail.com>
2023-05-06 11:42:49 +0200

    rooms filter process

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {useContext} from 'react';
+import {RoomContext} from '../context';
+import Title from "../components/Title"; 
 
 export default function RoomsFilter() {
+  const context = useContext(RoomContext);
+  const {
+    handleChange, type, capaxity, price, minPrice, maxPrice,inSize,maxSize,breakfast,pets
+  } = context;
+  
+  
   return (
-    <div>
-      Hello from rooms filter
-    </div>
+    <section className="filter-continer">
+     <Title title="Encuentra tu casa rural ideal" />
+     <form className="filter-form">
+      {/*select type */}
+      {/* */}
+     </form>
+    </section>
   )
 }
