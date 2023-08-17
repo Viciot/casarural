dee3c8afd447a2286cc2d725ed9c6ba019779181
Viciot <vfuentesnoguera@gmail.com>
2023-08-17 14:38:31 +0200

    changes in roomsFilter

--- src/context.js
+++ src/context.js
@@ -24,63 +24,70 @@ class RoomProvider extends Component {
 
 // getData
 
-componentDidMount(){
-  // this.getData
-  let rooms = this.formatData(items);
-  let featuredRooms = rooms.filter(room => room.featured === true);
-  let maxPrice = Math.max(...rooms.map(item => item.price));
-  let maxSize = Math.max(...rooms.map(item => item.size));
+  componentDidMount(){
+    // this.getData
+    let rooms = this.formatData(items);
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
   
-  this.setState({
-    rooms, 
-    featuredRooms, 
-    sortedRooms:rooms, 
-    loading:false,
-    price:maxPrice,
-    maxPrice,
-    maxSize     
-  });  
-}
+    this.setState({
+      rooms, 
+      featuredRooms, 
+      sortedRooms:rooms, 
+      loading:false,
+      price:maxPrice,
+      maxPrice,
+      maxSize     
+    });  
+  }
 
-formatData(items){
-  let tempItems = items.map(item =>{
-    let id = item.sys.id
-    let images = item.fields.images.map(image => image.fields.file.url);
+  formatData(items){
+    let tempItems = items.map(item =>{
+      let id = item.sys.id
+      let images = item.fields.images.map(image => image.fields.file.url);
 
-    let room = {...item.fields,images,id}
+      let room = {...item.fields,images,id}
+      return room;
+
+    });
+    return tempItems
+  }
+
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find(room => room.slug === slug);
     return room;
+  };
+  handleChange = event => {
+    const target = event.target
+    const value = event.type === "checkbox" ?
+    target.checked:target.value
+    const name = event.target.name
+    this.setState(
+      {
+        [name]:value
+      },
+      this.filterRooms
+    );
+  };
+  filterRooms = () => {
+    let{
+      rooms, type, capacity, price, minSize, maxSize, breakfast,
+      pets
+    } = this.state
 
-  });
-  return tempItems
-}
-
-getRoom = (slug) => {
-  let tempRooms = [...this.state.rooms];
-  const room = tempRooms.find(room => room.slug === slug);
-  return room;
-};
-handleChange = event => {
-  const target = event.target
-  const value = event.type === "checkbox" ?
-  target.checked:target.value
-  const name = event.target.name
-  this.setState({
-    [name]:value
-  },this.filterRooms)
-  
-
-};
-
-filterRooms = () =>{
-  console.log("hello");
-};
-
+    let tempRooms = [...rooms];
+  };
   render() {
     return (
-        <RoomContext.Provider value={{...this.state, 
-          getRoom: this.getRoom,handleChange:this.handleChange
+        <RoomContext.Provider 
+          value={{
+            ...this.state, 
+          getRoom: this.getRoom,
+          handleChange:this.handleChange
           }}>                     
-            {this.props.children}
+          {this.props.children}
         </RoomContext.Provider>
     );
   }
@@ -89,7 +96,8 @@ filterRooms = () =>{
 
 const RoomConsumer = RoomContext.Consumer;
 
-//Esto es un higher order component... ni guarra vamos, pero gracias a esto envias los datos a traves del roomConsumer con el value
+/*Esto es un higher order component... ni guarra vamos, pero gracias a esto
+envias los datos a traves del roomConsumer con el value*/
 export function withRoomConsumer (Component){
   return function ConsumerWrapper(props){
     return <RoomConsumer>
